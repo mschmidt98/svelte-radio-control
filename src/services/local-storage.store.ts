ed5cf947bb1ef75b8643cc3c503dac018d246afb
Markus S <m.estrich98@gmail.com>
2020-12-06 18:55:40 +0100

    API-Url in localStorage speichern

--- src/services/local-storage.store.ts
+++ src/services/local-storage.store.ts
@@ -3,10 +3,17 @@ import { injectable } from "tsyringe";
 @injectable()
 export class LocalStorageService {
     private readonly DEFAULT_SERVICE_ROUTE = 'http://volumio/';
+    private readonly STORAGE_URL_KEY = 'service_url';
 
     constructor() {}
 
     public getServiceUrl(): string {
+        const storedUrl = localStorage[this.STORAGE_URL_KEY];
+        if (storedUrl) {
+            return storedUrl;
+        }
+
+        localStorage[this.STORAGE_URL_KEY] = this.DEFAULT_SERVICE_ROUTE;
         return this.DEFAULT_SERVICE_ROUTE;
     }
 }
