62f927adabe2ceb823f2f3072071d3cac6755feb
Markus S <m.estrich98@gmail.com>
2020-12-15 23:22:13 +0100

    App.svelte verschoben

---
+++ src/components/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import FavoriteList from "./FavoriteList.svelte";
+import NowPlaying from "./NowPlaying.svelte";
+
+</script>
+
+<main>
+	<p>
+		Konzept: Zwei Tabs
+	</p>
+		<ul>
+			<li>Meine Favoriten</li>
+			<li>Sendersuche</li>
+		</ul>
+
+	<p>
+		Unterhalb der Tabs immer 'aktuelle Wiedergabe anzeigen'
+	</p>
+	<FavoriteList />
+
+	<NowPlaying />
+</main>
+
+<style>
+</style>
