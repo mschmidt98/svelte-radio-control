7561f33530799c9b05b0a2fe9ac28b4acf497b5a
Markus S <m.estrich98@gmail.com>
2020-12-19 15:28:00 +0100

    Nice lookin' mobile nav

--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -1,12 +1,16 @@
 <nav class="bg-gray-800">
-    <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+    <div class="max-w-7xl mx-auto px-3 sm:px-4 lg:px-8">
         <div class="flex items-center justify-between h-14">
-            <div class="flex-shrink-0">
-                <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
+            <div class="flex items-center">
+                <div class="flex-shrink-0">
+                    <img class="h-8 w-8" src="../assets/icons/icon-192_white.png" alt="Volumio Icon">
+                </div>
+                <div class="text-gray-300 text-xl mx-5">
+                    Radio Control
+                </div>
             </div>
             <div>
-                <a class="text-gray-300 px-3 py-2 rounded-md text-sm text-xl"
-                   href="#">
+                <a href="#" class="text-gray-300 px-2 py-2 rounded-md text-sm text-xl">
                     <i class="fas fa-wrench"></i>
                 </a>
             </div>
