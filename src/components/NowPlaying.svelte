3867e123dea49255df4857ac28c3332d7bb03a88
Schmidt, Markus <markus.schmidt@connext.de>
2020-12-23 18:05:17 +0100

    Added svelte material UI, with failing slider....

--- src/components/NowPlaying.svelte
+++ src/components/NowPlaying.svelte
@@ -2,15 +2,21 @@
     import { container } from 'tsyringe';
     import type { CurrentState } from '../models';
     import { VolumioStore } from '../services/volumio.store';
+    // import { Slider } from '@smui/slider/bare';
+    // import '@smui/slider/bare.css';
+    import Button from '@smui/button';
+    import Checkbox from '@smui/checkbox';
 
     const service = container.resolve(VolumioStore);
     let currentStatePromise: Promise<CurrentState>;
+    let isChecked: boolean = true;
 
     function refreshState() {
         currentStatePromise = service.getCurrentState();
     }
 
     refreshState();
+    let volume: number = 100;
 </script>
 
 <div class="bg-gray-600 py-2">
@@ -20,7 +26,7 @@
     {:then currentState}
 
         <div class="flex">
-            <div class="rounded px-2">
+            <div class="rounded px-2 mt-2">
                 <img class="rounded-xl" height="145" width="145" alt="Albumcover" src="{currentState.albumart}"/>
             </div>
             <div class="text-md text-gray-300 leading-7">
@@ -36,6 +42,11 @@
             '{exception.config.url}':
             {exception.message}</p>
     {/await}
+
+<!--    <Slider bind:value={volume} />-->
+    <Button on:click={() => refreshState()}>MATERIAL</Button>
+
+    <Checkbox bind:checked={isChecked}>Auto Refresh</Checkbox>
 
     <button class="text-base font-medium rounded-lg p-3 bg-rose-500 text-white" on:click={() => refreshState()}>
         Refresh
